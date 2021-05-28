a97f34c4a43926b8fdf2d73da447461893edfd58
Kripijenius <18523147@students.uii.ac.id>
2021-05-28 14:57:51 +0700

    update-layout

---
+++ app/src/main/java/com/example/myapplication/splash/SplashActivity.kt
@@ -0,0 +1,21 @@
+package com.example.myapplication.splash
+
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.os.Handler
+import com.example.myapplication.MainActivity
+import com.example.myapplication.R
+
+class SplashActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?){
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_splash)
+        supportActionBar?.hide()
+        Handler(mainLooper).postDelayed({
+            val intent = Intent(this@SplashActivity, MainActivity::class.java)
+            startActivity(intent)
+            finish()
+        }, 3000)
+    }
+}
