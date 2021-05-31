e5ad7bca546d26d8519e3cc0ef72d75070309195
toymanjr31 <toymanjr31@gmail.com>
2021-05-31 10:49:59 +0700

    Update Detect Indication

--- app/src/main/java/com/example/myapplication/MainActivity.kt
+++ app/src/main/java/com/example/myapplication/MainActivity.kt
@@ -127,8 +127,6 @@ class MainActivity : AppCompatActivity(), UploadRequestBody.UploadCallback {
         val imgName = imgLinkParsed[3]
 
         activityMainBinding.textView.text = getString(R.string.detection_process)
-        activityMainBinding.progressBar.visibility = View.VISIBLE
-        activityMainBinding.progressBar.progress = 0
         ApiService().getResult(
             imgName
         ).enqueue(object : Callback<ResultResponse>{
@@ -142,8 +140,6 @@ class MainActivity : AppCompatActivity(), UploadRequestBody.UploadCallback {
                     Glide.with(this@MainActivity)
                         .load("http://10.0.2.2:8000$resultPath")
                         .into(activityMainBinding.imageView)
-                    activityMainBinding.progressBar.progress = 100
-                    activityMainBinding.progressBar.visibility = View.INVISIBLE
                     activityMainBinding.textView.text = getString(R.string.complete_detection)
                 }
             }
